cd1927080951d082b61987504f41b6a4a5652c3d
naylach <50495002+naylach@users.noreply.github.com>
2021-04-09 00:50:06 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tateti/MainActivity2.java
@@ -0,0 +1,33 @@
+package com.example.tateti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+
+        Button buttonStart = findViewById(R.id.button_comenzar);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
+    }
+
+    private void startGame() {
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
+    }
+}
